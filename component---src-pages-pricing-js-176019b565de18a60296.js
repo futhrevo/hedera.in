779b0977d1697685790581692e57660149150f1b
Rakesh Kalyankar <k.rakeshlal@gmail.com>
2021-01-20 07:42:06 +0530

    Updates

---
+++ component---src-pages-pricing-js-176019b565de18a60296.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[11],{azEF:function(e,n,t){"use strict";var a=t("q1tI"),l=t.n(a),r=t("DDoQ"),c=t("CyKR");n.a=function(e){var n=e.children;return l.a.createElement(c.a,null,l.a.createElement(r.c,{id:"top"},l.a.createElement(r.a,null,n)))}},u52P:function(e,n,t){"use strict";t.r(n);var a=t("q1tI"),l=t.n(a),r=t("azEF");n.default=function(){return l.a.createElement(r.a,null,l.a.createElement("h1",null,"Pricing"),l.a.createElement("div",null,"Coming soon"))}}}]);
+//# sourceMappingURL=component---src-pages-pricing-js-176019b565de18a60296.js.map
